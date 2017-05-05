5b7e37e73c210975610b92e9b47c34fe444cd6f1
Your Name <you@example.com>
2017-05-05 12:12:29 +0800

    前端和和后端的登录注册以及后端用户的增删

--- Application/Home/Controller/CategoryController.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Home\Controller;
-use Common\Controller\AuthController;
-use Think\Controller;
-class CategoryController extends AuthController {
-    public function index(){
-        $this->display();
-    }
-}
